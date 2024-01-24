e397695c1c8cd88fea98516b865fc769d8cf9985
Riley-Strandlund <FFOrstrandlund@gmail.com>
2024-01-24 03:52:09 -0600

    created camel.py file

--- PyWarmup1.py
+++ PyWarmup1.py
@@ -3,12 +3,12 @@ from datetime import datetime # Allows the use of dates and time
 print('Hello! Please Enter your name and Month that you were born in.')
 
 
-#get name and birth month
+# get name and birth month.
 Name = input("Name:")
 BirthMonth = input("Month:\n")
 nameLen = len(Name)
 today = datetime.now()
-monthName = today.strftime("%B")#converts datetime original format to month into string format time. Website docs help: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+monthName = today.strftime("%B")# converts datetime original format to month into string format time. Website docs help: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
 
 
 print(f"Hello, {Name}\n")
